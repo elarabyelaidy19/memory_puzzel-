fd98ea0d7a4092cd6d0c3fb4fbdcd9fecc1bc69f
elaraby elaidy <elarabyelaidy@gmail.com>
2021-04-17 03:22:19 +0200

    game class

--- game.rb
+++ game.rb
@@ -51,4 +51,13 @@ class Game
     sleep(1) 
     board.render 
   end 
-  
+
+  def play 
+    until board.won? 
+      board.render 
+      pos = get_player_input 
+      make_guess(pos)  
+    end 
+
+    puts "Congratulations, you win!" 
+  end 
